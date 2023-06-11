fc87b34ab00d2c31a3bd4c9a28b9caee6b990d19
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-06-11 21:38:47 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=perf kugo Kernel by dibin
+kernel.string=whatwursts Kernel by dibin
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=kugo
-device.name2=
+device.name1=maple_dsds
+device.name2=maple
 device.name3=
 device.name4=
 device.name5=
